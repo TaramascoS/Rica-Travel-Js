eeedb3811aa70cc0de7316c4835a16d35393cb9f
Taramasco Silvina <taramasco.silvina@gmail.com>
2023-07-03 23:58:00 -0300

    Primera Preentrega

---
+++ codigo.js
@@ -0,0 +1,38 @@
+alert("Bienvenida/o a Rica Rica Travel Agencia de Turismo 🌄🛩️🛻");
+
+let gastoTotal=0;
+
+let tour = prompt ("1- Laguna Cejar + Ojos del Salar (Valor 230 USD) \n 2- Geiser del Tatio (Valor 100 USD) \n 3- Valle Arcoiris (Valor 50 USD) \n 4- Tour Astronómico (Valor 50 USD) \n 0- Presione para finalizar");
+
+while(tour!="0"){
+    switch(tour){
+        case"1":
+            alert("Elegiste realizar el Tour Laguna Cejar + Ojos del Salar 🗻");
+            incrementarGastoTotal (230);
+            break;
+        case"2":
+            alert("Elegiste realizar el Tour Geiser del Tatio 🏔️");
+            incrementarGastoTotal (100);
+            break;
+        case"3":
+            alert("Elegiste realizar el Tour Valle Arcoiris 🌈");
+            incrementarGastoTotal (50);
+            break;
+        case"4":
+            alert("Elegiste realizar el Tour Astronómico 🌒🌠");
+            incrementarGastoTotal (50);
+            break; 
+        default:
+            alert("Código ingresado inválido 😵‍💫");
+            break;
+    }
+    tour = prompt ("1- Laguna Cejar + Ojos del Salar (Valor 230 USD) \n 2- Geiser del Tatio (Valor 100 USD) \n 3- Valle Arcoiris (Valor 50 USD) \n 4- Tour Astronómico (Valor 50 USD) \n 0- Presione para finalizar");
+}
+alert("Monto total por compra de Tours USD "+gastoTotal);
+
+function incrementarGastoTotal (precioTour){
+    gastoTotal = gastoTotal + precioTour;
+    console.log("Subtotal acumulado USD"+gastoTotal);
+}
+
+alert("Gracias por confiar en Rica Rica Travel Agencia de Turismo 👋👋");
