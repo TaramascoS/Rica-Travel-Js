0bd0cf235967849f66cf01c2db88d9f9069e18ee
Taramasco Silvina <taramasco.silvina@gmail.com>
2023-08-07 12:08:58 -0300

    Preentrega 3

--- excursiones.js
+++ excursiones.js
@@ -1,73 +1,73 @@
 const excursiones = [{
-    id: 1,
-    foto: "https://static.nationalgeographic.es/files/styles/image_3200/public/reflection-salar-de-uyuni-bolivia.webp?w=900&h=507",
-    nombre: "Laguna Cejar + Ojos del Salar",
-    precio: 230,
-},
-{
-    id: 2,
-    foto: "https://sajamaexpediciones.com/data/atacama/fa14d2589b0aaa6adac627ace36403b5.jpeg",
-    nombre: "Piedras Rojas + Lagunas",
-    precio: 100,
-},
-{
-    id: 3,
-    foto: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/bc/57/a7/valle-arcoiris.jpg?w=1200&h=-1&s=1",
-    nombre: "Valle Arcoiris",
-    precio: 50,
-},
-{
-    id: 4,
-    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4HiYrGnVCKUGPEerWivRm96W-mXRaoMavIlXHAVGfyA&s",
-    nombre: "Baltinache",
-    precio: 50,
-},
-{
-    id: 5,
-    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXAPPodmP7YIZYQK7BhWiCBodz6mbkk4BOK1iTYoL7zQBBsHqW8nvPFGi4mqU9YgX5PYQ&usqp=CAU",
-    nombre: "Geiser del Tatio",
-    precio: 100,
-},
-{
-    id: 6,
-    foto: "https://denomades.imgix.net/destinos/san-pedro-de-atacama/132/salar-de-tara-id132-2-es.jpg",
-    nombre: "Ruta de los Salares",
-    precio: 60,
-},
-{
-    id: 7,
-    foto: "https://www.tangol.com/Fotos/Tours/termas-de-puritama_1549_201807060659411.JPG",
-    nombre: "Termas de Puritama - AM",
-    precio: 75,
-},
-{
-    id: 8,
-    foto: "https://d2g6vh54b4p2bp.cloudfront.net/wp-content/uploads/2022/04/puritama-Politicas-cambios.jpg",
-    nombre: "Termas de Puritama - PM",
-    precio: 75,
-},
-{
-    id: 9,
-    foto: "https://i1.wp.com/gouyuni.com/wp-content/uploads/2021/09/IMG_9271-scaled.jpg?fit=2048%2C2560&ssl=1",
-    nombre: "Termas de Puripobre",
-    precio: 55,
-},
-{
-    id: 10,
-    foto: "https://denomades.imgix.net/destinos/san-pedro-de-atacama/1130/vallecito.jpg",
-    nombre: "Vallecito",
-    precio: 45,
-},
-{
-    id: 11,
-    foto: "https://www.porquemegustaviajar.com/wp-content/uploads/2021/06/Cascada-Escondida-San-Pedro-de-Atacama.jpeg",
-    nombre: "Cascada Escondida",
-    precio: 50,
-},
-{
-    id: 12,
-    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJbBwamZS0tKLQAR2tqGPKWn87E7jwTPqH30N-vaeZ3FzL-jsDhgv6yLe0feOY7fyeY6s&usqp=CAU",
-    nombre: "Tour Astronomico",
-    precio: 30,
-},
-]
+        id: 1,
+        foto: "https://static.nationalgeographic.es/files/styles/image_3200/public/reflection-salar-de-uyuni-bolivia.webp?w=900&h=507",
+        nombre: "Laguna Cejar + Ojos del Salar",
+        precio: 230
+    },
+    {
+        id: 2,
+        foto: "https://sajamaexpediciones.com/data/atacama/fa14d2589b0aaa6adac627ace36403b5.jpeg",
+        nombre: "Piedras Rojas + Lagunas",
+        precio: 100
+    },
+    {
+        id: 3,
+        foto: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/bc/57/a7/valle-arcoiris.jpg?w=1200&h=-1&s=1",
+        nombre: "Valle Arcoiris",
+        precio: 50
+    },
+    {
+        id: 4,
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4HiYrGnVCKUGPEerWivRm96W-mXRaoMavIlXHAVGfyA&s",
+        nombre: "Baltinache",
+        precio: 50
+    },
+    {
+        id: 5,
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXAPPodmP7YIZYQK7BhWiCBodz6mbkk4BOK1iTYoL7zQBBsHqW8nvPFGi4mqU9YgX5PYQ&usqp=CAU",
+        nombre: "Geiser del Tatio",
+        precio: 100
+    },
+    {
+        id: 6,
+        foto: "https://denomades.imgix.net/destinos/san-pedro-de-atacama/132/salar-de-tara-id132-2-es.jpg",
+        nombre: "Ruta de los Salares",
+        precio: 60
+    },
+    {
+        id: 7,
+        foto: "https://www.tangol.com/Fotos/Tours/termas-de-puritama_1549_201807060659411.JPG",
+        nombre: "Termas de Puritama - AM",
+        precio: 75
+    },
+    {
+        id: 8,
+        foto: "https://d2g6vh54b4p2bp.cloudfront.net/wp-content/uploads/2022/04/puritama-Politicas-cambios.jpg",
+        nombre: "Termas de Puritama - PM",
+        precio: 75,
+    },
+    {
+        id: 9,
+        foto: "https://i1.wp.com/gouyuni.com/wp-content/uploads/2021/09/IMG_9271-scaled.jpg?fit=2048%2C2560&ssl=1",
+        nombre: "Termas de Puripobre",
+        precio: 55,
+    },
+    {
+        id: 10,
+        foto: "https://denomades.imgix.net/destinos/san-pedro-de-atacama/1130/vallecito.jpg",
+        nombre: "Vallecito",
+        precio: 45,
+    },
+    {
+        id: 11,
+        foto: "https://www.porquemegustaviajar.com/wp-content/uploads/2021/06/Cascada-Escondida-San-Pedro-de-Atacama.jpeg",
+        nombre: "Cascada Escondida",
+        precio: 50,
+    },
+    {
+        id: 12,
+        foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJbBwamZS0tKLQAR2tqGPKWn87E7jwTPqH30N-vaeZ3FzL-jsDhgv6yLe0feOY7fyeY6s&usqp=CAU",
+        nombre: "Tour Astronomico",
+        precio: 30,
+    }
+];
--- logica.js
+++ logica.js
@@ -1,13 +1,144 @@
 console.table(excursiones);
+//si quedo un carro abandonado lo recupero y lo guardo en el array, sino inicializo el array vacio
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+let contenedor = document.getElementById("misprods");
+let cantidad = document.getElementById('cantidad');
+cantidad.innerText = `🛒${carrito.length}`;
+
+//primer renderizado
+if (carrito.length != 0) {
+    for (const prod of carrito) {
+        //agregar fila a la tabla de carrito
+        document.getElementById('tablabody').innerHTML += `
+            <tr>
+                <td>${prod.id}</td>
+                <td>${prod.nombre}</td>
+                <td>${prod.precio}</td>
+                <td><button class='btn btn-light'>🗑️</button></td>
+            </tr>
+        `;
+    }
+    //incrementar el total
+    let totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    document.getElementById('total').innerText = 'Total a pagar $: ' + totalCarrito;
+}
+
+
+
+//luxon
+const DateTime = luxon.DateTime;
+const inicio = DateTime.now();
+console.log(inicio.toLocaleString());
+console.log(inicio.weekdayLong);
+Swal.fire(`Que tengas un hermoso día ${inicio.weekdayLong}`);
+console.log(inicio.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS));
+
+
+
+function renderizarProductos(excursiones) {
+    contenedor.innerHTML = '';
+    for (const producto of excursiones) {
+        contenedor.innerHTML += `
+            <div class="card col-sm-2">
+                <img src=${producto.foto} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.id}</h5>
+                    <p class="card-text">${producto.nombre}</p>
+                    <p class="card-text">$ ${producto.precio}</p>
+                    <button id='btn${producto.id}' class="btn btn-primary align-bottom">Comprar</button>
+                </div>
+            </div>   
+        `;
+    }
+
+    //EVENTOS
+    excursiones.forEach((producto) => {
+        document.getElementById(`btn${producto.id}`).addEventListener('click', () => {
+            agregarACarrito(producto);
+        });
+    });
+}
+
+renderizarProductos(excursiones);
+
+function agregarACarrito(prodAAgregar) {
+    carrito.push(prodAAgregar);
+    cantidad.innerText = `🛒${carrito.length}`;
+    console.table(carrito);
+    //alert(`Agregaste ${prodAAgregar.nombre} al carrito ! 💪`);
+
+    //con sweetAlert2
+    Swal.fire({
+        title: 'Fantástico🥳!',
+        text: `Agregaste ${prodAAgregar.nombre} al carrito`,
+        imageUrl: prodAAgregar.foto,
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: prodAAgregar.nombre,
+    })
+
+    //agregar fila a la tabla de carrito
+    document.getElementById('tablabody').innerHTML += `
+        <tr>
+            <td>${prodAAgregar.id}</td>
+            <td>${prodAAgregar.nombre}</td>
+            <td>${prodAAgregar.precio}</td>
+            <td><button class='btn btn-light'>🗑️</button></td>
+        </tr>
+    `;
+    //incrementar el total
+    let totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    document.getElementById('total').innerText = 'Total a pagar $: ' + totalCarrito;
+    //localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+//finalizar compra
+
+let finalizarBtn = document.getElementById('finalizar');
+
+finalizarBtn.onclick = () => {
+    Toastify({
+        text: "Gracias por tu compra! Recibirás los datos de la reserva en 48hs.",
+        duration: 3000,
+        gravity: 'top',
+        position: 'right',
+        close: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        offset: {
+            x: 150, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 110 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+    }).showToast();
+    //vaciar el carro y la tabla
+    carrito = [];
+    document.getElementById('tablabody').innerHTML = ''
+    cantidad.innerText = `🛒${carrito.length}`;
+    document.getElementById('total').innerText = 'Total a pagar $: ';
+    localStorage.removeItem('carrito');
+    //luxon
+    const fin = DateTime.now();
+    const Interval = luxon.Interval;
+    const tiempoParaComprar = Interval.fromDateTimes(inicio, fin);
+    console.log(tiempoParaComprar.length('seconds'));
+}
+
+
+/*SEGUNDA PREENTREGA
+console.table(excursiones);
 /*let contenedorExcs=document.getElementById('misExc');*/
 //funcion para filtrar las excursiones por precio maximo
+/*
 function filtrarPorPrecio(precioMax){
     const filtrados = excursiones.filter((excursion) => excursion.precio<=precioMax);
-    /*console.log(filtrados);*/
+    /*console.log(filtrados);
     return filtrados;
-}
+}*/
 /*prueba de filtro que anda
-filtrarPorPrecio(80);*/
+filtrarPorPrecio(80);
 let precioUsuario = parseFloat(prompt("Ingresa el precio máximo a abonar para la excursión 🌄 (0-Salir)"));
 
 while (precioUsuario != 0){
@@ -16,17 +147,8 @@ while (precioUsuario != 0){
     }else{
         const excFiltrados = filtrarPorPrecio(precioUsuario);
         console.table(excFiltrados);
-        /*renderizarExcursiones(excFiltrados);*/
+        /*renderizarExcursiones(excFiltrados);
     } 
     precioUsuario = parseFloat(prompt("Ingresa el precio máximo a abonar para la excursión 🌄(0-Salir)"));
-}
-
-//DOM
-/*
-function renderizarExcursiones(listaExc){
-    for(const excursion of listaExc){
-        contenedorExcs.innerHTML += `
-        `;
-    }
 }
 */
